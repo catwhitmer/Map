2f386656ef5e99edfd7f453a415a1f24a5232fce
catwhitmer <catwhitmer@gmail.com>
2020-11-30 12:42:59 -0500

    Query selected needed elements and added eventListner

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyCOHl40h1_yh-tQ4SWjwTqLNJF54HKAons"
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // send this to Google API
+}
+
+form.addEventListener("submit", searchAddressHandler);
